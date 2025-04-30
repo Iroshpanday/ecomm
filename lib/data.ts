cb6ae2e47182002944727e7aa63041b099a3fb78
IroshPanday <iroshpanday@gmail.com>
2025-04-30 06:54:52 +0545

    created website layout (First stage)

---
+++ lib/data.ts
@@ -0,0 +1,40 @@
+//data.ts file in your Next.js ecommerce application serves the purpose of centralizing and organizing static data that is used across your components. Specifically, it defines the structure for your header navigation menu.
+
+const data = {
+  headerMenus: [
+    {
+      name: "Today's Deal",
+      href: '/search?tag=todays-deal',
+    },
+    {
+      name: 'New Arrivals',
+      href: '/search?tag=new-arrival',
+    },
+    {
+      name: 'Featured Products',
+      href: '/search?tag=featured',
+    },
+    {
+      name: 'Best Sellers',
+      href: '/search?tag=best-seller',
+    },
+    {
+      name: 'Browsing History',
+      href: '/#browsing-history',
+    },
+    {
+      name: 'Customer Service',
+      href: '/page/customer-service',
+    },
+    {
+      name: 'About Us',
+      href: '/page/about-us',
+    },
+    {
+      name: 'Help',
+      href: '/page/help',
+    },
+  ],
+}
+
+export default data
